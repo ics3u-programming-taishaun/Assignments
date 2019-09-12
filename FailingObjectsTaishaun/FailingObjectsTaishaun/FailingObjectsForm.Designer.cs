415fcf4a4b4c0388e069585aa37a1e3f096dd80a
TaishaunJ <47784239+TaishaunJ@users.noreply.github.com>
2019-09-12 12:45:51 -0400

    update

---
+++ FailingObjectsTaishaun/FailingObjectsTaishaun/FailingObjectsForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace FailingObjectsTaishaun
+{
+    partial class FailingObjectsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FailingObjectsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1173, 652);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "FailingObjectsForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
